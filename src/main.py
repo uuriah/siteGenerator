6ede830221d510bf42ed56aa4aa304ea020d2739
uuriah <132951774+uuriah@users.noreply.github.com>
2026-02-07 13:31:06 -0800

    Call copy contents routines

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from textnode import TextNode
 from textnode import TextType
+from copy_contents import remove_public_content, copy_content
 
 def main():
     textNode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
 
     print(textNode)
+
+    remove_public_content("./public")
+    copy_content("./static", [])
 
 if __name__ == "__main__":
     main()
