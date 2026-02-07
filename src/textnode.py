53a9c6f7318bf3f35069b8e6ba4653ed6ddd0d64
uuriah <132951774+uuriah@users.noreply.github.com>
2026-02-07 13:34:01 -0800

    Update textnode

--- src/textnode.py
+++ src/textnode.py
@@ -30,21 +30,20 @@ def text_node_to_html_node(text_node):
     if text_node.text_type.value == "text":
         return LeafNode(None, text_node.text)
 
-    elif text_node.text_type.value == "bold":
+    if text_node.text_type.value == "bold":
         return LeafNode("b", text_node.text)
     
-    elif text_node.text_type.value == "italic":
+    if text_node.text_type.value == "italic":
         return LeafNode("i", text_node.text)
     
-    elif text_node.text_type.value == "code":
+    if text_node.text_type.value == "code":
         return LeafNode("code", text_node.text)
     
-    elif text_node.text_type.value == "link":
+    if text_node.text_type.value == "link":
         return LeafNode("a", text_node.text, {"href": text_node.url})
     
-    elif text_node.text_type.value == "image":
+    if text_node.text_type.value == "image":
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     
-    else:
-        raise Exception("Invalid Text Type")
+    raise ValueError(f"invalid text type: {text_node.text_type}")
     
