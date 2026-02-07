805aec7b7b746ef3c314198c771b7021eec15c38
uuriah <132951774+uuriah@users.noreply.github.com>
2026-02-07 13:30:27 -0800

    Script to copy files from static dir to public dir

---
+++ src/copy_contents.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+
+def remove_public_content(root_dir):
+    dir_content = os.listdir(root_dir)
+
+    if not dir_content:
+        return
+
+    for item in dir_content:
+        full_path = root_dir + "/" + item
+        if os.path.isfile(full_path):
+            os.remove(full_path)
+        else:
+            shutil.rmtree(full_path)
+    return 
+
+def copy_content(current_dir, copied):
+    dir_content = os.listdir(current_dir)
+
+    for item in dir_content:
+        full_path = current_dir + "/" + item
+        if os.path.isdir(full_path):
+
+            if full_path not in copied:
+                copy_content(full_path, copied)
+
+        elif os.path.isfile(full_path):
+            filename = os.path.basename(full_path)
+            
+            directories = full_path.replace(item, "")
+            nested_dirs = directories.replace("./static", "")
+            if nested_dirs != "/":
+                destination_path = os.path.join("./public/" + nested_dirs, filename)
+                os.makedirs("./public/" + nested_dirs, exist_ok=False)
+                shutil.copy(full_path, destination_path)
+            else:
+                shutil.copy(full_path, "./public")
+            copied.append(full_path)
+    return copied
+
+
